91a64deb398422c58aed68767af498c6c869bc52
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-09-01 01:39:45 +0200

    Import GameEvents + RangedFloats

---
+++ Assets/ScriptsFriedemann/TestEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : MonoBehaviour {
+
+    public Transform player;
+    public GameObject 
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(player);
+    }
+
+    private void Shoot()
+    {
+
+    }
+}
