9aa9fb903fbc8fcf022e68780b99e4173a0a198b
MariaMen <Ma_Men@gmx.de>
2018-09-01 20:07:42 +0200

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public Button playButton;
+    public Button closeButton;
+    public Button creditsButton;
+    public Button quitApplicationCloseQuestionButton;
+    public Button closeCloseQuestionPanel;
+
+    public GameObject closeQuestionPanel;
+
+
+    public void PlayButton()
+    {
+       // SceneManager.LoadScene(SCENENAME);
+    }
+	
+    public void CloseButton()
+    {
+        closeQuestionPanel.SetActive(true);
+    }
+
+    public void CloseQuestionQuit()
+    {
+        closeQuestionPanel.SetActive(false);
+    }
+
+    public void CreditsButton()
+    {
+      //  SceneManager.LoadScene(SCENENAME);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+	
+}
