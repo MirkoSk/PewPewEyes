dacc8042415c6b2b7928a9a6d049ef20ea26ba8c
MariaMen <Ma_Men@gmx.de>
2018-09-01 23:29:01 +0200

    Credits implemented

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+    public string menuScene;
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+}
