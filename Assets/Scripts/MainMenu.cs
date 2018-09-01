35ecc420897eff27410e48e38fddc29e6889547a
MariaMen <Ma_Men@gmx.de>
2018-09-01 20:46:00 +0200

    Main Menu Funktion

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,6 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    public Button playButton;
-    public Button closeButton;
-    public Button creditsButton;
-    public Button quitApplicationCloseQuestionButton;
-    public Button closeCloseQuestionPanel;
 
     public GameObject closeQuestionPanel;
 
@@ -19,19 +14,9 @@ public class MainMenu : MonoBehaviour {
        // SceneManager.LoadScene(SCENENAME);
     }
 	
-    public void CloseButton()
-    {
-        closeQuestionPanel.SetActive(true);
-    }
-
-    public void CloseQuestionQuit()
-    {
-        closeQuestionPanel.SetActive(false);
-    }
-
     public void CreditsButton()
     {
-      //  SceneManager.LoadScene(SCENENAME);
+      //  SceneManager.LoadScene(Credits);
     }
 
     public void QuitApplication()
