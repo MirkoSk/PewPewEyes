5d5ea8bce39d316b88411ec2da3344d3a1fc35ea
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-09-02 16:25:09 +0200

    SpawnManager

---
+++ Assets/Scripts/Scriptable Objects/EnemyWave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+/// 
+[CreateAssetMenu(menuName = "Scriptable Objects/Enemy Wave")]
+public class EnemyWave : ScriptableObject
+{
+
+    public int enemyAmount;
+
+    public GameObject tpye1Enemy;
+    [Range(0, 1)]
+    public float type1Ratio;
+    public GameObject type2Enemy;
+    [Range(0, 1)]
+    public float type2Ratio;
+
+    public float spawnTime;
+
+    [HideInInspector] public int type1Amount;
+    [HideInInspector] public int type2Amount;
+}
