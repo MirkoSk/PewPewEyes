d4b7459435f16517094cc27873cc8485b2003daf
Mirko <mirkoskroch@gmail.com>
2018-09-02 15:55:22 +0200

    ChargeLaser ParticleSystem and timing

---
+++ Assets/Scripts/Enemies/ShootLaserFromAnimator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+/// <summary>
+/// 
+/// </summary>
+public class ShootLaserFromAnimator : MonoBehaviour 
+{
+
+    #region Variable Declarations
+    // Serialized Fields
+    [SerializeField] StateMachine stateMachine;
+    [SerializeField] GameObject shootState;
+	// Private
+	
+	#endregion
+	
+	
+	
+	#region Public Properties
+	
+	#endregion
+	
+	
+	
+	#region Unity Event Functions
+	private void Start () 
+	{
+        
+	}
+	#endregion
+	
+	
+	
+	#region Public Functions
+	public void ShootLaser()
+    {
+        stateMachine.ChangeState(shootState);
+    }
+	#endregion
+	
+	
+	
+	#region Private Functions
+
+	#endregion
+	
+	
+	
+	#region Coroutines
+	
+	#endregion
+}
